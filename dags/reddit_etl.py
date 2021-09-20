151c8474eb5e3b88f09cc5a8c547f90cba1df477
Ulises Ortega <ulises.ortega2937@alumnos.udg.mx>
2021-09-19 21:09:44 -0500

    Correción de rutas

--- dags/reddit_etl.py
+++ dags/reddit_etl.py
@@ -3,23 +3,22 @@ import pandas as pd
 import praw
 from sqlalchemy import create_engine
 
+
 def check_if_valid_data(df: pd.DataFrame) -> bool:
+    '''Revisa que los datos sean válidos.'''
     # Dataframe vacío
     if df.empty:
-        print("Sin datos. Fin de la ejecución")
-        return False 
+        print('Sin datos.')
+        return False
 
     # Unicidad de llaves primarias
-    if pd.Series(df['ID']).is_unique:
+    if pd.Series(df['id']).is_unique:
         pass
     else:
-        raise Exception("Llave primaria repetida")
-
-    # Valores nulos
-    if df.isnull().values.any():
-        raise Exception("Valores nulos encontrados")
+        raise Exception('Llave primaria repetida')
 
     return True
+
 
 def run_reddit_etl():
     # Autenticación de Reddit:
@@ -36,22 +35,23 @@ def run_reddit_etl():
 
     # Por cuestiones de seguridad, las credenciales
     # se guardan en un archivo externo
-    filename = 'client_secrets.json'
+
+    FILENAME = 'client_secrets.json'
+    DATABASE_LOCATION = 'sqlite:///reddit_dataset.db'
+
     try:
-        with open(filename, 'r') as f:
+        with open(FILENAME, 'r') as f:
             creds = json.load(f)
     except FileNotFoundError:
-        exit(f'no se encontró el archivo {filename}, fin de la ejecución')
+        exit(f'no se encontró el archivo {FILENAME}, fin de la ejecución')
 
-    reddit = praw.Reddit(   
+    reddit = praw.Reddit(
         client_id=creds.get('client_id'),
         client_secret=creds.get('client_secret'),
         user_agent=creds.get('user_agent'),
         redirect_uri=creds.get('redirect_uri'),
         refresh_token=creds.get('refresh_token')
     )
-
-    DATABASE_LOCATION = 'sqlite:///reddit_dataset.db'
 
     # Datos que serán scrapeados
     author_list = []
@@ -66,38 +66,40 @@ def run_reddit_etl():
     subreddit = reddit.subreddit('unixporn')
     hot_post = subreddit.hot(limit=10000)
     for sub in hot_post:
-        author_list.append(sub.author)
+        author_list.append(str(sub.author))
         id_list.append(sub.id)
         link_flair_text_list.append(sub.link_flair_text)
         num_comments_list.append(sub.num_comments)
         score_list.append(sub.score)
         title_list.append(sub.title)
         upvote_ratio_list.append(sub.upvote_ratio)
-    
+
     print(subreddit, 'completado; ', end='')
     print('total', len(author_list), 'posts han sido scrapeados')
-    
-    # Obtención del dataframe
-    df = pd.DataFrame({'ID':id_list, 
-                   'Author':author_list, 
-                   'Title':title_list,
-                   'Count_of_Comments':num_comments_list,
-                   'Upvote_Count':score_list,
-                   'Upvote_Ratio':upvote_ratio_list,
-                   'Flair':link_flair_text_list
-                  })
+
+    # Se crea el dataframe
+    df = pd.DataFrame(
+        {'id': id_list,
+         'author': author_list,
+         'title': title_list,
+         'count_of_comments': num_comments_list,
+         'upvote_count': score_list,
+         'upvote_ratio': upvote_ratio_list,
+         'flair': link_flair_text_list
+         })
 
     # Validar
     if check_if_valid_data(df):
         print('Datos válidos. Inicia proceso Load')
-    
+    else:
+        exit('No son datos válidos')
     # Load
     engine = create_engine(DATABASE_LOCATION, echo=False)
     try:
         with engine.begin() as connection:
-            df.to_sql('unixporn', con=connection, if_exists='append', index=False)
-    except:
-        print(f'Datos existentes en la base de datos {DATABASE_LOCATION}')
-    
-    print("Base de datos actualizada")
-
+            df.to_sql('unixporn', con=connection,
+                      if_exists='append', index=False)
+        print('Base de datos actualizada')
+    except Exception as e:
+        exit(
+            f'Error: base de datos no actualizada. Razón {e.with_traceback(None)}')
